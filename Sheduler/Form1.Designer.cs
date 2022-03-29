3470c2c3f74c73b6e81bc267a26fe93e990168bf
DoOoMB <92219016+DoOoMB@users.noreply.github.com>
2022-03-29 10:03:28 +0300

    Создание узлов в xml файле.

---
+++ Sheduler/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿
+namespace Sheduler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AddNewNoteButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // AddNewNoteButton
+            // 
+            this.AddNewNoteButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.AddNewNoteButton.Location = new System.Drawing.Point(0, -1);
+            this.AddNewNoteButton.Name = "AddNewNoteButton";
+            this.AddNewNoteButton.Size = new System.Drawing.Size(799, 42);
+            this.AddNewNoteButton.TabIndex = 0;
+            this.AddNewNoteButton.Text = "+";
+            this.AddNewNoteButton.UseVisualStyleBackColor = true;
+            this.AddNewNoteButton.Click += new System.EventHandler(this.AddNewNoteEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.AddNewNoteButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button AddNewNoteButton;
+    }
+}
+
---
+++ Sheduler/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace Sheduler
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void AddNewNoteEvent(object sender, EventArgs e)
+        {
+            XmlManager.CreateNewNote(name: "NewNote", text: "YourText"); // Создаём новую заметку
+        }
+    }
+
+    public static class XmlManager
+    {
+        public static void CreateNewNote(string name, string text) //метод для создания заметок
+        {
+            XmlDocument xDoc = new XmlDocument(); 
+            xDoc.Load("NotesList.xml"); // загружем xml-файл
+            XmlElement rNode = xDoc.DocumentElement; // вытаскиваем корневой узел
+
+            // Структуру xml-документа смотри в шаблоне
+
+
+            // Создание узлов
+            XmlElement note = xDoc.CreateElement("note"); 
+            XmlAttribute noteNameAttr = xDoc.CreateAttribute("name");
+            XmlElement noteText = xDoc.CreateElement("noteText");
+
+            // Создание текстовых значений для атрибутов и узлов
+            XmlText noteTextVal = xDoc.CreateTextNode(text);
+            XmlText noteNameAttrVal = xDoc.CreateTextNode(name);
+
+            // Добавление значений
+            noteNameAttr.AppendChild(noteNameAttrVal);
+            noteText.AppendChild(noteTextVal);
+
+            // Создание узлов
+            note.Attributes.Append(noteNameAttr);
+            note.AppendChild(noteText);
+
+            // Добавляем новую заметку в корневой узел
+            rNode.AppendChild(note);
+
+            // Сохраняем изменения
+            xDoc.Save("NotesList.xml");
+;        }
+    }
+}
